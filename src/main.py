9f9674111f4ea585d128fffd23122ae9e66a8cea
Aaditey Nim <aaditeynim@gmail.com>
2026-01-09 22:46:24 +0530

    Added screenshots and demo

--- src/main.py
+++ src/main.py
@@ -1,20 +1,65 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
 import pandas as pd
 from data_cleaning import clean_data
 from analysis import generate_summary
 from report_generator import format_excel
+import os
+import sys
 
-input_file = "input_files/raw_data.xlsx"
-output_file = "output_files/final_report.xlsx"
 
-df = clean_data(input_file)
+def get_base_dir():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    return os.path.dirname(os.path.abspath(__file__))
 
-summary, top = generate_summary(df)
 
-with pd.ExcelWriter(output_file, engine="openpyxl") as writer:
-    df.to_excel(writer, sheet_name="Clean Data", index=False)
-    summary.to_excel(writer, sheet_name="Summary", index=False)
-    top.to_excel(writer, sheet_name="Top 5 Transactions", index=False)
+def run_automation():
+    file_path = filedialog.askopenfilename(
+        filetypes=[("Excel files", "*.xlsx")]
+    )
 
-format_excel(output_file)
+    if not file_path:
+        return
 
-print("Report generated successfully.")
+    try:
+        df = clean_data(file_path)
+        summary, top = generate_summary(df)
+
+        base_dir = get_base_dir()
+        output_dir = os.path.join(base_dir, "output_files")
+        os.makedirs(output_dir, exist_ok=True)
+
+        output_path = os.path.join(output_dir, "final_report.xlsx")
+
+        with pd.ExcelWriter(output_path, engine="openpyxl") as writer:
+            df.to_excel(writer, sheet_name="Clean Data", index=False)
+            summary.to_excel(writer, sheet_name="Summary", index=False)
+            top.to_excel(writer, sheet_name="Top 5 Transactions", index=False)
+
+        format_excel(output_path)
+
+        messagebox.showinfo("Success", f"Report generated successfully!\n\nSaved in:\n{output_path}")
+
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+
+app = tk.Tk()
+app.title("Excel Report Automation")
+app.geometry("420x220")
+app.resizable(False, False)
+
+label = tk.Label(app, text="Select an Excel file to generate report", font=("Arial", 12))
+label.pack(pady=25)
+
+btn = tk.Button(
+    app,
+    text="Choose Excel File & Generate Report",
+    command=run_automation,
+    height=2,
+    width=32
+)
+btn.pack()
+
+app.mainloop()
